a78a05178e4f6527b5209086998c2641d860c0cc
Victoria Geeks <victoriageeks@gmail.com>
2022-09-30 09:16:23 +0200

    ejercicios subidos

---
+++ helloReus/src/helloReus/ComentarioVariasLineas.java
@@ -0,0 +1,16 @@
+package helloReus;
+
+public class ComentarioVariasLineas {
+
+	public static void main(String[] args) {
+		/*System.out.prinln("Hola");
+		 System.out.println("Adios");
+		 System.out.println("Cucu");*/
+		for (int i=0;i<6;/*ahora viene el paso*/i++)
+			System.out.println(i+" ");
+		System.out.println("FIN DE PROGRAMA");
+			
+
+	}
+
+}
